6a9e8772bd1f247472cd70a8e134edcde0e3db95
TRISHAN00 <trishansaha43@gmail.com>
2022-07-06 01:24:45 +0600

    final update

--- app.js
+++ app.js
@@ -135,13 +135,13 @@
 // //   - Email
 // //  */
 
-// // function uuid(mask = "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx") {
-// //   return `${mask}`.replace(/[xy]/g, function (c) {
-// //     var r = (Math.random() * 16) | 0,
-// //       v = c == "x" ? r : (r & 0x3) | 0x8;
-// //     return v.toString(16);
-// //   });
-// // }
+// function uuid(mask = "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx") {
+//   return `${mask}`.replace(/[xy]/g, function (c) {
+//     var r = (Math.random() * 16) | 0,
+//       v = c == "x" ? r : (r & 0x3) | 0x8;
+//     return v.toString(16);
+//   });
+// }
 
 // // const employers = [
 // //   {
