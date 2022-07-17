c043dd90bca3f2a024a424f9d667c58387bf15fb
TRISHAN00 <trishansaha43@gmail.com>
2022-07-17 16:06:47 +0600

    eval two number of string

--- function.js
+++ function.js
@@ -41,4 +41,18 @@ function footballPoints(wins, draws, losses) {
   return wins * 3 + draws * 1 + losses * 0;
 }
 
-console.log(footballPoints(0, 0, 1));
+// Create a function that will handle simple math expressions. The input is an expression in the form of a string.
+
+// calculator("23+4") ➞ 27
+
+// calculator("45-15") ➞ 30
+
+// calculator("13+2-5*2") ➞ 5
+
+// calculator("49/7*2-3") ➞ 11
+
+function calculator(str) {
+  return eval(str);
+}
+
+console.log(calculator("45-15"));
