fee2a5dbac07253b3ef85df61aebce1fca1c00d1
TRISHAN00 <trishansaha43@gmail.com>
2022-07-29 16:27:29 +0600

    make a function that input a various value in array and return only true values

--- lodash.js
+++ lodash.js
@@ -3,3 +3,14 @@ const chunk = (arr, size) => {
   for (let i = 0; i < arr.length; i += size) {}
   return result;
 };
+
+// compact
+const compact = (arr) => {
+  const result = arr.reduce((acc, curr) => {
+    if (curr) {
+      acc.push(curr);
+    }
+    return acc;
+  }, []);
+  return result;
+};
