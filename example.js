8c9f8613c07f1ba4a505f7efc70628686d49f3c6
TRISHAN00 <trishansaha43@gmail.com>
2022-08-02 13:55:37 +0600

    get comments using posts id

--- example.js
+++ example.js
@@ -5,9 +5,16 @@ const POST_URL = `https://jsonplaceholder.typicode.com/posts`;
 const COMMENTS_URL = `https://jsonplaceholder.typicode.com/comments`;
 
 async function getComments(username) {
-  const { data: user } = await axios.get(`${USERS_URL}?username=${username}`);
-  const { data: post } = await axios.get(`${POST_URL}?userId=${user[0].id}`);
-  console.log(post);
+  try {
+    const { data: user } = await axios.get(`${USERS_URL}?username=${username}`);
+    const { data: posts } = await axios.get(`${POST_URL}?userId=${user[0].id}`);
+    const { data: comments } = await axios.get(
+      `${COMMENTS_URL}?postId=${posts[0].id}`
+    );
+    console.log(comments);
+  } catch (e) {
+    console.log(e);
+  }
 }
 
 getComments("Bret");
