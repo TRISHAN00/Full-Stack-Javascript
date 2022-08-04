9f3a643eaca0774957465e6849a2e5e6d2a6ee6c
TRISHAN00 <trishansaha43@gmail.com>
2022-08-04 16:54:17 +0600

    white function example

--- Iterators.js
+++ Iterators.js
@@ -1,16 +1,23 @@
-const arr = [1, 2, 3, 4, 5];
 let index = 0;
 function next(arr) {
   return arr[index++];
 }
 
 // Second example of iterator [Symbol]
-const channel = "Love";
+const channel = "Love Big";
 const channelIterator = channel[Symbol.iterator]();
-console.log(channelIterator.next());
-console.log(channelIterator.next());
+// console.log(channelIterator.next());
+// console.log(channelIterator.next());
 
 // Iterate a string using for of loop
 for (const value of channel) {
-  console.log(value);
+  //   console.log(value);
+}
+
+while (true) {
+  const data = channelIterator.next();
+  if (data.done) {
+    break;
+  }
+  //   console.log(data.value);
 }
