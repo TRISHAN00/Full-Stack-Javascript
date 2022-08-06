2851f07a42fd2e9480e7d6cb3f48f3d2ed1150d2
TRISHAN00 <trishansaha43@gmail.com>
2022-08-06 15:46:08 +0600

    get data usng axios

--- generator.js
+++ generator.js
@@ -1,3 +1,19 @@
+const axios = require("axios").default;
+
+async function getUsers() {
+  const url = `https://jsonplaceholder.typicode.com/users`;
+  const { data: users } = await axios.get(url);
+  return users;
+}
+
+getUsers()
+  .then((user) => {
+    console.log(user);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
 function* generatorId() {
   let index = 1;
   while (true) {
@@ -8,12 +24,14 @@ function* generatorId() {
 const generateUserId = generatorId();
 const generateProfileId = generatorId();
 
-console.log("User", generateUserId.next().value);
-console.log("User", generateUserId.next().value);
-console.log("User", generateUserId.next().value);
-console.log("User", generateUserId.next().value);
+// console.log("User", generateUserId.next().value);
+// console.log("User", generateUserId.next().value);
+// console.log("User", generateUserId.next().value);
+// console.log("User", generateUserId.next().value);
 
-console.log("Product Id", generateProfileId.next().value);
-console.log("Product Id", generateProfileId.next().value);
-console.log("Product Id", generateProfileId.next().value);
-console.log("Product Id", generateProfileId.next().value);
+// console.log("Product Id", generateProfileId.next().value);
+// console.log("Product Id", generateProfileId.next().value);
+// console.log("Product Id", generateProfileId.next().value);
+// console.log("Product Id", generateProfileId.next().value);
+
+// Difference between Async Iterator and async generator
